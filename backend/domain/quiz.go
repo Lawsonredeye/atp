f331ec75894e81055b85ccc1dcf9d8965a7e0af3
lawsonredeye <omoregbeeolawson@gmail.com>
2026-01-03 23:07:32 +0100

    feat: Add QuestionsData struct for quiz question management

--- backend/domain/quiz.go
+++ backend/domain/quiz.go
@@ -7,3 +7,10 @@ type QuizResponse struct {
 	Answer          string
 	Explanation     string
 }
+
+type QuestionsData struct {
+	Name        string   `json:"name"`
+	Options     []string `json:"options"`
+	Answer      string   `json:"answer"`
+	Explanation string   `json:"explanation"`
+}
